b5c050a3f3841e855ad56557873c2fe7027672ad
KanaeNarimatsu <patamanegi44@gmail.com>
2016-10-23 14:51:07 +0900

    test3
    test3

---
+++ GihubTest/Assets/GithubScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GithubScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+       aaaaa
+	
+	}
+	
+  
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
